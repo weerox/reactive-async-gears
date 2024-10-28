93ea978af228778a32a107bbbea85633e497a8f7
Aron Bergman <bathingrad@gmail.com>
2024-10-28 19:15:31 +0100

    Make State covariant

--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -1,6 +1,6 @@
 package rasync
 
-sealed trait State[V]
+sealed trait State[+V]
 case class Intermediate[V](value: V) extends State[V]
 case class Completed[V](value: V) extends State[V]
 case class Failed(exception: Throwable) extends State
